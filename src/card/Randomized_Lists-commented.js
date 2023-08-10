c6669d67ee62cc51f09b38a7066e7f40243e9df2
Huandney Ferreira Vaz <huandney@gmail.com>
2023-08-10 01:24:12 -0400

    Create Randomized_Lists-commented.js

---
+++ src/card/Randomized_Lists-commented.js
@@ -0,0 +1,43 @@
+<script data-name="Randomized Lists" data-version="v2.0.0">
+// https://github.com/huandney/Anki-Insert-Randomized-Lists
+
+// Function to reorder the items of a list based on the provided indices.
+function reorderList(ul, indices) {
+    indices.forEach(index => ul.appendChild(ul.children[index]));
+}
+
+// Function to shuffle the elements of an array.
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Main function to handle the randomization and reordering of lists.
+function run() {
+    // Select all lists that need to be shuffled
+    var ulElements = document.querySelectorAll('ul.shuffle');
+    // Check if it's on the back of the card.
+    var isBack = !!document.getElementById('back');
+    // Retrieve all stored indices or initialize an empty object.
+    var allIndices = isBack ? JSON.parse(sessionStorage.getItem('allIndices')) : {};
+
+    ulElements.forEach((ul, i) => {
+        // Use stored indices or shuffle to get new indices.
+        var indices = allIndices[i] || shuffleArray(Array.from({length: ul.children.length}, (_, idx) => idx));
+        // If on the front, store the generated indices.
+        if (!isBack) allIndices[i] = indices;
+        // Reorder the list based on indices.
+        reorderList(ul, indices);
+    });
+
+    // If on the front, store all indices in sessionStorage.
+    if (!isBack) sessionStorage.setItem('allIndices', JSON.stringify(allIndices));
+}
+
+// Run the main function
+run();
+
+</script>
