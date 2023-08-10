84a9cd8166373c7a3f74b5ab939a3ba1e4808259
Huandney Ferreira Vaz <huandney@gmail.com>
2023-08-10 19:34:44 -0400

    Update randomized_lists-commented.js

--- src/card/randomized_lists-commented.js
+++ src/card/randomized_lists-commented.js
@@ -1,29 +1,17 @@
 // Function to reorder the items of a list based on the provided indices.
 function reorderList(ul, indices) {
-    // For each index in the 'indices' array
-    indices.forEach(index => {
-        // Append the child element at the given index to the end of the 'ul' list.
-        // This effectively reorders the items based on the sequence of indices.
-        ul.appendChild(ul.children[index]);
-    });
+    indices.forEach(index => ul.appendChild(ul.children[index]));
 }
 
 // Function to shuffle the elements of an array.
 function shuffleArray(array) {
-    // Start the loop from the last element of the array
     for (var i = array.length - 1; i > 0; i--) {
-        // Generate a random index between 0 and i (inclusive)
         var j = Math.floor(Math.random() * (i + 1));
-        
-        // Swap the elements at positions i and j
         [array[i], array[j]] = [array[j], array[i]];
     }
-    
-    // Return the shuffled array
     return array;
 }
 
-// Main function to handle the randomization and reordering of lists.
 function run() {
     // Select all lists that need to be shuffled
     var ulElements = document.querySelectorAll('ul.shuffle');
